044a5f1680e906bba03dc99c2857916fc27b27c7
abderrahim <abderrahim.amzaourou@gmail.com>
2023-10-05 00:23:38 +0100

    where were i ?

---
+++ two_sum/src/main.rs
@@ -0,0 +1,3 @@
+fn main() {
+    println!("where were i?");
+}
---
+++ two_sum/target/debug/deps/Two_Sum-8bac6ee9921aa78d.d
@@ -0,0 +1,5 @@
+/home/amz/grind-75-leet-code-in-Rust/Two_Sum/target/debug/deps/Two_Sum-8bac6ee9921aa78d.rmeta: src/main.rs
+
+/home/amz/grind-75-leet-code-in-Rust/Two_Sum/target/debug/deps/Two_Sum-8bac6ee9921aa78d.d: src/main.rs
+
+src/main.rs:
