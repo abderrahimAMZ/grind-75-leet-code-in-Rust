e2cb5345b07f9077f1a03dc5d59ac39ae84485cf
abderrahimAMZ <abderrahim.amzaourou@gmail.com>
2024-01-27 17:30:05 +0100

    solved 01-Matrix

---
+++ 01-Matrix/src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+        int[][] nums  = {{0,0,0},{0,1,0},{1,1,1}};
+        int[][] result = solution.updateMatrix(nums);
+
+        for (int i = 0; i < result.length; i++) {
+            for (int j = 0; j < result[i].length; j++) {
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
