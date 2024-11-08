d09978d5c2d082d23600c74b485ff1b06df2afa2
abderrahimAMZ <abderrahim.amzaourou@gmail.com>
2024-11-08 22:27:32 +0100

    done, search in sorted rotated array

---
+++ search-in-sorted-rotated-array/src/Main.java
@@ -0,0 +1,43 @@
+
+
+
+
+
+
+
+
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+        // test the code in the Solution class
+        Solution solution = new Solution();
+
+
+        // write the three tests
+        int[] nums = {5,1,3};
+
+        int target = 5;
+        System.out.println(solution.search(nums, target)); // 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
